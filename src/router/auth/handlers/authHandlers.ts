1fe8aa183719d690571a5ee2b454e0b9da44d3f4
ihorzharyi <ihor.zh@zazmic.com>
2020-04-20 23:32:29 +0300

    fix auth middleware

--- src/router/auth/handlers/authHandlers.ts
+++ src/router/auth/handlers/authHandlers.ts
@@ -1,14 +1,19 @@
 import { AuthService } from '../../../services/auth';
 
 export const login = async (ctx: any): Promise<void> => {
-    const authService = new AuthService();
+    try {
+        const authService = new AuthService();
 
-    const user = {
-        email: ctx.request.body.email,
-        password: ctx.request.body.password
-    };
+        const user = {
+            email: ctx.request.body.email,
+            password: ctx.request.body.password
+        };
 
-    ctx.response.body = await authService.login(user);
+        ctx.response.body = await authService.login(user);
+    } catch (e) {
+        ctx.response.body = JSON.stringify(e);
+        ctx.response.status = 502;
+    }
 };
 
 export const register = async (ctx: any): Promise<void> => {
