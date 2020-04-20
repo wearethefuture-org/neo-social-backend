8ad3476c0c541d7a4b85884eba3e9162c5140ff8
ihorzharyi <ihor.zh@zazmic.com>
2020-04-20 23:28:20 +0300

    fix auth middleware

--- src/enums/Urls/unauthorizedUrls.ts
+++ src/enums/Urls/unauthorizedUrls.ts
@@ -1,14 +1,14 @@
 export const unauthorizedUrls = [
     {
-        url: '/api/v1/login',
+        url: '/api/v1/auth/login',
         method: 'POST'
     },
     {
-        url: '/api/v1/register',
+        url: '/api/v1/auth/register',
         method: 'POST'
     },
     {
-        url: '/api/v1/confirm',
+        url: '/api/v1/auth/confirm',
         method: 'POST'
     }
 ];
