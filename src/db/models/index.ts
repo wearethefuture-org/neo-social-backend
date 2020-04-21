8d5d128b2f2e5fd010ec5450790b94cefe56ccd8
Bogdan Derevyaga <derevyaga.bogdan@fornova.com>
2020-04-21 22:29:55 +0300

    39 - Change architecture of db models

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -1,42 +1,33 @@
-import * as fs from 'fs';
-import * as path from 'path';
 import { Sequelize } from 'sequelize-typescript';
 
 import { aliases } from './aliases';
 import { relations } from './relations';
+import * as sources from './sources';
 
-const basename = path.basename(__filename);
 const db: any = {
-  aliases
+    aliases
 };
 
 const sequelize: any = new Sequelize(process.env.DATABASE_NAME, process.env.POSTGRES_USERNAME, process.env.POSTGRES_PASSWORD, {
-  host: process.env.POSTGRES_HOST,
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 });
 
-fs.readdirSync(`${__dirname}/sources`)
-    .filter(file =>
-        (
-            file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.ts'
-        ))
-    .forEach(file => {
-      const model: any = sequelize.import(path.join(__dirname, 'sources', file));
-      db[model.name] = model;
-    });
+[...Object.keys(sources)].forEach(modelName => {
+    db[modelName] = sequelize.import(modelName, Object(sources)[modelName]);
+});
 
-Object.keys(db)
-    .forEach(modelName => {
-      if (db[modelName].associate) {
+[...Object.keys(db)].forEach(modelName => {
+    if (db[modelName].associate) {
         db[modelName].associate(db);
-      }
-    });
+    }
+});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
@@ -44,5 +35,5 @@ db.Sequelize = Sequelize;
 relations(db);
 
 export {
-  db as Model
+    db as Model
 };
